b19efe5be3b7c24dd9512b4e69f187755f220ddc
ivanwillsau <ivanwillsau@df482c85-7f0f-0410-bf46-9d4e49b368ba>
2006-03-25 13:48:26 +0000

    Displaying of the code is now working. Also fixed up some html bugs
    git-svn-id: https://docperl.svn.sourceforge.net/svnroot/docperl/trunk/docperl@9 df482c85-7f0f-0410-bf46-9d4e49b368ba

--- DocPerl/Cached/CODE.pm
+++ DocPerl/Cached/CODE.pm
@@ -112,32 +112,175 @@ use version;
 use Carp;
 use Data::Dumper qw/Dumper/;
 use Scalar::Util;
-use base qw/Exporter/;
+use base qw/DocPerl::Cached/;
 
 our $VERSION = version->new('0.0.1');
 our @EXPORT = qw//;
 our @EXPORT_OK = qw//;
 
 
-=head3 C<sub ( $search,  )>
+=head3 C<process ( $var1, $var2,  )>
 
-Param: C<$search> - type (detail) - description
+Param: C<$var1> - type (detail) - description
 
-Return: DocPerl::Cached::CODE - 
+Param: C<$var2> - type (detail) - description
+
+Return:  - 
 
 Description: 
 
 =cut
 
-sub new {
-	my $caller = shift;
-	my $class  = (ref $caller) ? ref $caller : $caller;
-	my %param  = @_;
-	my $self   = \%param;
+sub process {
+	my $self	= shift;
+	my $source	= $self->{source} || '';
 	
-	bless $self, $class;
+	return $self->syntax_highlight($source);
+}
+
+sub syntax_highlight {
+	my $self		= shift;
+	my $q			= $self->{cgi};
+	my ($code)		= @_;
+	my $in_pod		= 0;
+	my $here_marker	= '';
+	my $here_style;
+	my @lines;
 	
-	return $self;
+	open my $file, '<', $code or warn "Could not open the file '$code': $!" and return [];
+	
+	while ( my $line = <$file> ) {
+		# convert tabs into spaces of width 4
+		my $code_line = toSpaces( $line, 4 );
+		
+		my $j         = $.;
+		my $line_no   = qq{<a name="line$j" class="line_no"> $j </a> };# $q->a( { -name => "line$j", -class => "line_no" }, " $j " ) . "  ";
+		
+		# Quote all special characters
+		$code_line =~ s/&/&amp;/gxs;
+		$code_line =~ s/</&lt;/gxs;
+		$code_line =~ s/>/&gt;/gxs;
+		
+		# check if the line we are currently passing is a here doc line
+		if ( $here_marker ) {
+			#$code_line = qq{<span class="pod">$code_line}; #$q->start_span( { -class => "pod" } ) . $code_line;
+		}
+		
+		# check if we are currently processing POD
+		if ( $in_pod || $code_line =~ /^=/ ) {
+			$in_pod = $code_line =~ /^=cut/ ? 0 : 1;
+			$code_line = qq{<span class="pod">$code_line}; #$q->start_span( { -class => "pod" } ) . $code_line;
+		}
+		elsif ( $code_line and not $here_marker ) {
+			# check if the line has a comment
+			my ( $code, $char, $comment ) = $code_line =~ /(?:^ | (.*?) ([^\$]) ) # (.*)$/xs;
+			
+			# check if the line is that sort of line
+			if ( not $code and not $char and not $comment ) {
+				$code    = $code_line;
+				$char    = '';
+				$comment = '';
+			}
+			elsif ($comment) {
+				$comment = qq{<span class="comment">#$comment</span>}; #$q->span( { -class => "comment" }, "#$comment" );
+			}
+			
+			# tag the various parts of perl code
+			if ($code) {
+				# tag opperators
+				$code =~ s/(=~?|\.=|\|\|=|\*=|\\=|\+=|-=|\|\|?|&amp;(?:&amp;)?|<=>)/<span class="operator">$1<\/span>/gxs;
+				# tag string opperators
+				$code =~ s/([^\w&])(eq|ne|le|ge|lt|gt|cmp)(\W)/$1<span class="operator">$2<\/span>$3/gxs;
+				# tag opperators missed by other re's
+				$code =~ s/([^-])(&lt;|&gt;)/$1<span class="operator">$2<\/span>/gxs;
+				# tag reserved words
+				$code =~ s/(\W|^)(if|else|elsif|unless|while|do|for|foreach|sub|return|my|our|local|use|require|no)(\W|$)/$1<span class="reserved">$2<\/span>$3/gxs;
+				# tag builtin functions
+				$code =~ s/(\W|^)(shift|warn|die|exit|print|open|close|exists|defined)(\(|\s)/$1<span class="builtin">$2<\/span>$3/gxs;
+				# tag builtin functions
+				$code =~ s/(\W|^)(sort|keys|values|unlink|push|pop|shift|unshift)(\(|\s)/$1<span class="builtin">$2<\/span>$3/gxs;
+				# tab variable declerations
+				$code =~ s/(\W|^)(my|our|local)(\W)/$1<span class="declatory">$2<\/span>$3/gxs;
+				# tag loop operators
+				$code =~ s/(\W)(next|last|exit)(\W|$)/$1<span class="other">$2<\/span>$3/gxs;
+				# tag logical opperators
+				$code =~ s/(\s)(not|or\s+not|or|and\s+not|and)(\s)/$1<span class="logic">$2<\/span>$3/gxs;
+				$code =~ s/(->|\s)(new)(\(|\s)/$1<span class="builtin">$2<\/span>$3/gxs;
+			}
+			
+			# reassemble the line of code
+			$code_line = $code;
+			$code_line .= $char    if $char;
+			$code_line .= $comment if $comment;
+		}
+		
+		# Check if the line contains a heredoc reference
+		if ( $line and $line =~ /<<(['"])(\w+)\1/ ) {
+			$here_style  = $1;
+			$here_marker = $2;
+		}
+		
+		# close the pod span if one started
+		if ( $in_pod ) {
+			$code_line .= qq{</span>}; #$q->end_span();
+		}
+		
+		# mark the line as such
+		$code_line = qq{<span class="line">$code_line</span>}; #$q->span( { -class => "line" }, $code_line );
+		$code_line =~ s/(?:\r?\n)+//gxs;
+		
+		
+		# check if the here doc has ended
+		if ( $here_marker and $line and $line =~ /^$here_marker/ ) {
+			$here_marker = undef;
+		}
+		
+		# add the line to the array
+		push @lines, "$line_no$code_line";
+	}
+	
+	close $file;
+	
+	return lines => \@lines;
+}
+
+=head2 toSpaces ( $line, $tab_size )
+
+	@param	$line:		The line with tabs that are to be converted into spaces
+	@param	$tab_size:	The size that tabs are assumed to be
+	@return	string:		The line with tabs converted into spaces
+
+Converts one line of text from using tabs of size $tab_size to using spaces
+
+=cut
+
+sub toSpaces {
+	my ( $line, $tab_size ) = @_;
+
+	# only do the check if there are tabs
+	return $line unless $line =~ /\t/;
+	my $spaces = " " x $tab_size;
+	return $spaces if $line eq "\t";
+
+	# remove any initial spaces
+	$line =~ s/^(\s*)\t/$1$spaces/gxs while ( $line =~ /^(\s*)\t/ );
+
+	# split the line into tabs
+	my @parts = split /\t/, $line;
+	unshift @parts, '' if $line =~ /^\t/;
+	my $final = ( $line =~ /\t$/ ) ? 1 : 0;
+	$line = '';
+
+	for ( my $i = 0 ; $i < @parts ; $i++ ) {
+		my $part = $parts[$i];
+		unless ($part) {
+			$line .= $spaces;
+			next;
+		}
+		$line .= $part;
+		$line .= " " x ( 4 - length($part) % $tab_size ) if $i != $#parts or $final;
+	}
+	return $line;
 }
 
 
